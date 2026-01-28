0e4a6dad0c2729c96bf60705881188f64e331c90
Samik0007 <samikisdope07@gmail.com>
2026-01-28 06:23:30 +0545

    Add project files.

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace JournalPersonalApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
